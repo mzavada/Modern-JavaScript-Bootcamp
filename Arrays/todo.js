70cc526f34b9b88c4cd1355f8938702d9c494d98
Matthew Zavada <mattzavada@gmail.com>
2018-12-02 08:20:19 -0500

    Moving forward

---
+++ Arrays/todo.js
@@ -0,0 +1,7 @@
+const todos = ['Learn JavaScript', 'Workup Local Needs', 'Perpare for Elders Meeting', 'Sleep', 'Brush teeth']
+
+console.log(`
+You have ${todos.length} todos
+Todo #1: ${todos[0]}
+Todo #${todos.length-1}: ${todos[todos.length-2]}
+`)
