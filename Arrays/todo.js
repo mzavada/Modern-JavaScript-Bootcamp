ca86f1e7db6e14b2951fee322215ca3cd89f8a68
Matthew Zavada <mattzavada@gmail.com>
2018-12-05 21:37:13 -0500

    Finished the Array section

--- Arrays/todo.js
+++ Arrays/todo.js
@@ -1,7 +1,81 @@
-const todos = ['Learn JavaScript', 'Workup Local Needs', 'Perpare for Elders Meeting', 'Sleep', 'Brush teeth']
+const todos = [{
+    text: 'Learn JavaScript',
+    completed: false
+}, {
+    text: 'Workup Local Needs',
+    completed: false
+}, {
+    text: 'Perpare for Elders Meeting',
+    completed: false
+}, {
+    text: 'Sleep',
+    completed: true
+}, {
+    text: 'Brush teeth',
+    completed: true
+}]
 
-console.log(`
-You have ${todos.length} todos
-Todo #1: ${todos[0]}
-Todo #${todos.length-1}: ${todos[todos.length-2]}
-`)
+function deleteTodo(array, text) {
+    const index = array.findIndex(function (todo) {
+        return todo.text.toLowerCase() === text.toLowerCase()
+    })
+    if (index > -1) {
+        array.splice(index, 1)
+    }
+}
+
+function removeCompleted(array) {
+    let index = array.length - 1
+
+    while (index >= 0) {
+        if (array[index].completed === true) {
+            array.splice(index, 1)
+        }
+        index -= 1
+    }
+}
+
+function getThingsToDo(list) {
+
+    return list.filter(item => !item.completed)
+
+}
+
+function sortTodos(list) {
+    list.sort(function (a, b) {
+        if (!a.completed && b.completed) {
+            return -1
+        } else if (!b.completed && a.completed) {
+            return 1
+        } else {
+            return 0
+        }
+    })
+}
+
+sortTodos(todos)
+console.log(todos)
+console.log(todos[1].completed)
+
+// console.log(getThingsToDo(todos))
+
+// deleteTodo(todos, 'brush teeth')
+// console.log(todos)
+// removeCompleted(todos)
+// console.log(todos)
+
+// todos.splice(2, 1)
+
+// todos.push("Go to Meeting")
+
+// todos.shift()
+
+// todos.forEach((item, index) => console.log(`${index + 1}. ${item}`))
+
+// for (let i = 0; i <= todos.length - 1; i++) {
+//     console.log(`${i + 1}. ${todos[i]}`)
+// }
+
+// for (let i = todos.length - 1; i >= 0; i--) {
+//     console.log(`${i + 1}. ${todos[i]}`)
+// }
