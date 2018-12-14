d8e6b1865aab36197babce4db56d31f5e10b009d
Matthew Zavada <mattzavada@gmail.com>
2018-12-13 20:35:59 -0500

    Finished section 9

--- Functions/grade-calculator.js
+++ Functions/grade-calculator.js
@@ -1,19 +1,29 @@
-const calcGrade = function (score, totalScore = 20) {
-    const finalScore = score / totalScore * 100;
-    let grade
+const calcGrade = (score, totalScore = 20) => {
+  // verify that both agruments are numbers
+  if (typeof score !== 'number' || typeof totalScore !== 'number') {
+    throw new Error('Both agruments should be a number');
+  }
 
-    if (finalScore >= 90) {
-        grade = "A"
-    } else if (finalScore >= 80) {
-        grade = "B"
-    } else if (finalScore >= 70) {
-        grade = "C"
-    } else if (finalScore >= 60) {
-        grade = "D"
-    } else {
-        grade = "F"
-    }
-    return `You got a ${grade} (${finalScore}%)!`
+  const finalScore = score / totalScore * 100;
+  let grade = '';
+
+  if (finalScore >= 90) {
+    grade = 'A';
+  } else if (finalScore >= 80) {
+    grade = 'B';
+  } else if (finalScore >= 70) {
+    grade = 'C';
+  } else if (finalScore >= 60) {
+    grade = 'D';
+  } else {
+    grade = 'F';
+  }
+  return `You got a ${grade} (${finalScore}%)!`;
+};
+
+
+try {
+  console.log(calcGrade(18, 25));
+} catch (e) {
+  console.log(e.message);
 }
-
-console.log(calcGrade(13))
